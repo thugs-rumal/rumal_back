ee50d108c94753aaca9ec53d290cbf12ac8e0af4
Dennis Parchkov <dennis.parchkov@gmail.com>
2016-05-27 15:58:33 +0100

    unbuffer workaround test

--- main/management/commands/run_thug.py
+++ main/management/commands/run_thug.py
@@ -207,6 +207,7 @@ class Command(BaseCommand):
     def run_task(self, task):
         # Initialize args list for docker
         args = [
+            "unbuffer",
             "/usr/bin/sudo", "/usr/bin/docker", "run",
             "--rm",
             "-a", "stdin",
