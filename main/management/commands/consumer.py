68847865bbd4625f91e2d4a402273bf97d770c98
Dennisparchkov <dp1u12@soton.ac.uk>
2016-06-12 19:09:29 +0200

    Return data from thug scan if complete sucessfully, or failed status and frontend id

--- main/management/commands/consumer.py
+++ main/management/commands/consumer.py
@@ -3,10 +3,14 @@ import pika
 import logging
 import thread
 import json
+from bson.objectid import ObjectId
 import yaml
 
 from django.core import serializers
 from django.utils.encoding import smart_str
+
+import pymongo
+import time
 
 from main.models import *
 
@@ -14,14 +18,36 @@ RPC_HOST = '0.0.0.0'
 RPC_PORT = 5672
 RPC_QUEUE = 'rpc_queue'
 
+NEW_SCAN_TASK = 1
+
+STATUS_NEW              = 0
+STATUS_PROCESSING       = 1
+STATUS_FAILED           = 2
+STATUS_COMPLETED        = 3
+
 logger = logging.getLogger(__name__)
+
+client = pymongo.MongoClient()
+db = client.thug
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        else:
+            return obj
 
 class Command(BaseCommand):
 
-
     def on_request(self, ch, method, props, body):
         body = yaml.safe_load(body)
-        print body
+        if(int(body["task"]) == NEW_SCAN_TASK):
+            body.pop("task")
+            self.new_task(ch, method, props, body)
+
+    def new_task (self, ch, method, props, body):
+        frontend_id = str(body['frontend_id'])
         logger.debug("Task received")
         dict = [{"model": "main.task",
           "fields" :
@@ -30,20 +56,38 @@ class Command(BaseCommand):
 
         dict = (json.dumps(dict))
         list  = smart_str(dict)
-        print list
         for obj in  serializers.deserialize('json', list):
             obj.save()
         logger.debug("Task saved")
 
-        self.reply(ch, method, props, body)
+
+        logger.info("Waiting for task to finish")
+
+
+        while ((Task.objects.get(frontend_id=frontend_id).status is STATUS_PROCESSING) or (Task.objects.get(frontend_id=frontend_id).status is STATUS_NEW)):
+            time.sleep(2)
+        logger.debug("Task Completed")
+        task = Task.objects.get(frontend_id=frontend_id)
+        task_status = task.status
+        if task_status == STATUS_COMPLETED:
+            result = db.analysiscombo.find({'frontend_id': frontend_id })
+            self.reply(ch, method, props, {"status": STATUS_COMPLETED,
+                                                      "data": result[0]
+                                                      })
+        if task_status == STATUS_FAILED:
+            self.reply(ch, method, props, {"status": STATUS_FAILED,
+                                                      "data": frontend_id
+                                                      })
+        logger.debug("Response sent")
 
 
     def reply(self, ch, method, props, body):
+
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
                          properties=pika.BasicProperties(correlation_id= \
                                                              props.correlation_id),
-                         body='test')
+                         body=json.dumps(body, cls=Encoder))
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
