0b0caccdff8c75a9ff32b6c99c535142a46ac4d3
Pietro Delsante <p.delsante@certego.net>
2016-06-29 23:41:42 +0200

    Fixing imports and making it pep8 compliant

--- main/management/commands/run_thug.py
+++ main/management/commands/run_thug.py
@@ -21,50 +21,58 @@
 #
 import logging
 import netifaces
-import os
 import re
 import signal
 import subprocess
 import time
 import tldextract
 import socket
+import pytz
 
 from datetime import datetime
 from urlparse import urlparse
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
-from main.models import *
+from django.core.management.base import BaseCommand
+from main.models import Task
 from main.utils import clone_without_object_ids
 
 import pymongo
 from bson import ObjectId
 
-STATUS_NEW              = 0
-STATUS_PROCESSING       = 1
-STATUS_FAILED           = 2
-STATUS_COMPLETED        = 3
+STATUS_NEW = 0
+STATUS_PROCESSING = 1
+STATUS_FAILED = 2
+STATUS_COMPLETED = 3
 
 client = pymongo.MongoClient()
 db = client.thug
 
 logger = logging.getLogger(__name__)
 
+
 class TimeoutException(Exception):
     pass
+
 
 class InvalidMongoIdException(Exception):
     pass
 
+
 def timeout_handler(signum, frame):
     raise TimeoutException
+
 
 class Command(BaseCommand):
 
     def _fetch_new_tasks(self):
-        return Task.objects.filter(status__exact=STATUS_NEW).order_by('submitted_on')
+        return Task.objects.filter(
+            status__exact=STATUS_NEW
+        ).order_by('submitted_on')
 
     def _reset_processing(self):
-        Task.objects.filter(status__exact=STATUS_PROCESSING).update(status=STATUS_NEW)
+        Task.objects.filter(
+            status__exact=STATUS_PROCESSING
+        ).update(status=STATUS_NEW)
 
     def _mark_as_running(self, task):
         logger.debug("[{}] Marking task as running".format(task.id))
@@ -84,62 +92,127 @@ class Command(BaseCommand):
         task.status = STATUS_COMPLETED
         task.save()
 
-    def urlid_to_url(self,document):
-        document["url"] = db.urls.find_one({"_id":ObjectId(document["url_id"])})["url"]
+    def urlid_to_url(self, document):
+        document["url"] = db.urls.find_one({
+            "_id": ObjectId(document["url_id"])
+        })["url"]
         document.pop("url_id")
         return document
 
-    def remove_id_analysis_id(self,document):
+    def remove_id_analysis_id(self, document):
         "also removes id"
         document.pop("analysis_id")
         document.pop("_id")
         return document
 
-    def remove_analysis_id(self,document):
+    def remove_analysis_id(self, document):
         "removes only analysis id"
         document.pop("analysis_id")
         return document
 
-    def id_to_old_id(self,document):
-        "removed because this can be done in a more clean manner in frontend"
-        pass
-    def club_collections(self,analysis_id):
-        analysis = db.analyses.find_one({"_id":ObjectId(analysis_id)})
-        analysis["exploits"] = [self.remove_id_analysis_id(self.urlid_to_url(x)) for x in db.exploits.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["codes"] = [self.remove_id_analysis_id(x) for x in db.codes.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["behaviors"] = [self.remove_id_analysis_id(x) for x in db.behaviors.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["certificates"] = [self.remove_id_analysis_id(self.urlid_to_url(x)) for x in db.certificates.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["maec11"] = [self.remove_id_analysis_id(x) for x in db.maec11.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["pcaps"] = [self.remove_id_analysis_id(x) for x in db.pcaps.find({"analysis_id":ObjectId(analysis_id)})]
-        first_url = db.urls.find_one({"_id":ObjectId(analysis["url_id"])})
+    def club_collections(self, analysis_id):
+        analysis = db.analyses.find_one({
+            "_id": ObjectId(analysis_id)
+        })
+
+        analysis["exploits"] = [
+            self.remove_id_analysis_id(self.urlid_to_url(x))
+            for x in db.exploits.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["codes"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.codes.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["behaviors"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.behaviors.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["certificates"] = [
+            self.remove_id_analysis_id(self.urlid_to_url(x))
+            for x in db.certificates.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["maec11"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.maec11.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["pcaps"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.pcaps.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+
+        first_url = db.urls.find_one({"_id": ObjectId(analysis["url_id"])})
         first_url["old_id"] = first_url.pop("_id")
-        analysis["url_map"] = [first_url] #for further grid_fs maps id to url
+        analysis["url_map"] = [first_url]  # for further grid_fs maps id to url
         analysis = self.urlid_to_url(analysis)
         analysis.pop("_id")
-        #now cleaning connections
-        #using urls instead of url_ids
-        connections = [self.remove_id_analysis_id(x) for x in db.connections.find({"analysis_id":ObjectId(analysis_id)})]
+
+        # Now cleaning connections
+        # Using urls instead of url_ids
+        connections = [
+            self.remove_id_analysis_id(x)
+            for x in db.connections.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+
         for x in connections:
             # x.pop("analysis_id")
             # x.pop()
-            x["source_url"] = db.urls.find_one({"_id":ObjectId(x["source_id"])})["url"]
-            temp = db.urls.find_one({"_id":ObjectId(x["destination_id"])})
+            x["source_url"] = db.urls.find_one({
+                "_id": ObjectId(x["source_id"])
+            })["url"]
+
+            temp = db.urls.find_one({
+                "_id": ObjectId(x["destination_id"])
+            })
+
             if temp not in analysis["url_map"]:
-                temp["old_id"]= temp.pop("_id")
+                temp["old_id"] = temp.pop("_id")
                 analysis["url_map"].append(temp)
+
             x["destination_url"] = temp["url"]
             x.pop("source_id")
             x.pop("destination_id")
 
         analysis["connections"] = connections
-        # ids or not removed only in case of the sample
-        analysis["samples"] = [self.remove_analysis_id(x) for x in db.samples.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["locations"] = [self.remove_id_analysis_id(x) for x in db.locations.find({"analysis_id":ObjectId(analysis_id)})]
 
-        analysis["virustotal"] = [self.remove_id_analysis_id(x) for x in db.virustotal.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["honeyagent"] = [self.remove_id_analysis_id(x) for x in db.honeyagent.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["androguard"] = [self.remove_id_analysis_id(x) for x in db.androguard.find({"analysis_id":ObjectId(analysis_id)})]
-        analysis["peepdf"] = [self.remove_id_analysis_id(x) for x in db.peepdf.find({"analysis_id":ObjectId(analysis_id)})]
+        # Only samples keep their original id, all other fields lose
+        # both id and analysis_id
+        analysis["samples"] = [
+            self.remove_analysis_id(x)
+            for x in db.samples.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["locations"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.locations.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["virustotal"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.virustotal.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["honeyagent"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.honeyagent.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["androguard"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.androguard.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
+        analysis["peepdf"] = [
+            self.remove_id_analysis_id(x)
+            for x in db.peepdf.find({
+                "analysis_id": ObjectId(analysis_id)
+            })]
 
         return analysis
 
@@ -147,22 +220,31 @@ class Command(BaseCommand):
         """ Resolves IP from given URL """
         ext = tldextract.extract(url)
         if ext.subdomain:
-            return socket.gethostbyname(ext.subdomain + "." + ext.registered_domain)  # ToDo: possibly check for exceptions
+            # ToDo: possibly check for exceptions
+            return socket.gethostbyname(
+                ext.subdomain + "." + ext.registered_domain
+            )
         else:
             return socket.gethostbyname(ext.registered_domain)
 
     def make_flat_tree(self, analysis, analysis_id):
         logger.info("Now making flat tree.")
-        root_url_id = db.connections.find({"analysis_id": ObjectId(analysis_id)}).sort("chain_id")[0]['source_id']
+
+        root_url_id = db.connections.find({
+            "analysis_id": ObjectId(analysis_id)
+        }).sort("chain_id")[0]['source_id']
+
         root_url = db.urls.find_one({"_id": root_url_id})
+
         # flat_tree_nodes holds the tree in the form of a list
         # of nodes with each node having a node id(nid or index in list)
         # and a parent parameter which refers to parent node
         # url_id is unique
-        flat_tree_nodes = [{"url_id": root_url_id,
-                            "parent": None,
-                            "url": root_url
-                            }]  # root node as initial element
+        flat_tree_nodes = [{
+            "url_id": root_url_id,
+            "parent": None,
+            "url": root_url
+        }]  # root node as initial element
 
         # Traverses through flat_tree_nodes
         # initially consisting only of root
@@ -171,11 +253,37 @@ class Command(BaseCommand):
         # to parent are added.
         # Checks for cycles before adding
         for nid, node in enumerate(flat_tree_nodes):
-            url = db.urls.find_one({"_id": ObjectId(node["url_id"])})
-            locations    = clone_without_object_ids(db.locations.find_one({"analysis_id": ObjectId(analysis_id), "url_id": ObjectId(node["url_id"])}) or {})
-            samples    = [clone_without_object_ids(x, 'sample_id') for x in db.sampless.find({"analysis_id": ObjectId(analysis_id), "url_id": ObjectId(node["url_id"])})]
-            exploits    = [clone_without_object_ids(x) for x in db.exploits.find({"analysis_id": ObjectId(analysis_id), "url_id": ObjectId(node["url_id"])})]
-            certificates = [clone_without_object_ids(x) for x in db.certificates.find({"analysis_id": ObjectId(analysis_id), "url_id": ObjectId(node["url_id"])})]
+            url = db.urls.find_one({
+                "_id": ObjectId(node["url_id"])
+            })
+
+            locations = clone_without_object_ids(
+                db.locations.find_one({
+                    "analysis_id": ObjectId(analysis_id),
+                    "url_id": ObjectId(node["url_id"])
+                }) or {}
+            )
+
+            samples = [
+                clone_without_object_ids(x, 'sample_id')
+                for x in db.sampless.find({
+                    "analysis_id": ObjectId(analysis_id),
+                    "url_id": ObjectId(node["url_id"])
+                })]
+
+            exploits = [
+                clone_without_object_ids(x)
+                for x in db.exploits.find({
+                    "analysis_id": ObjectId(analysis_id),
+                    "url_id": ObjectId(node["url_id"])
+                })]
+
+            certificates = [
+                clone_without_object_ids(x)
+                for x in db.certificates.find({
+                    "analysis_id": ObjectId(analysis_id),
+                    "url_id": ObjectId(node["url_id"])
+                })]
 
             node["url"] = url["url"]
             node["domain"] = url and urlparse(url['url']).hostname or '-',
@@ -190,20 +298,24 @@ class Command(BaseCommand):
             node['nid'] = nid
             flat_tree_nodes[nid] = node
 
-            for x in db.connections.find({"analysis_id": ObjectId(analysis_id),"source_id": ObjectId(node["url_id"])}):
+            for x in db.connections.find({
+                "analysis_id": ObjectId(analysis_id),
+                "source_id": ObjectId(node["url_id"])
+            }):
                 destination_id = x["destination_id"]
-                #  checks for cycles
-                if len([True for i, _ in enumerate(flat_tree_nodes) if _['url_id'] == destination_id]) > 0:
-                    continue # Do not add node to tree
-                #  No cycle found add to tree
-                temp = {
-                "url_id" : destination_id,
-                "parent" : nid
-                }
-                flat_tree_nodes.append(temp)
+
+                # Checks for cycles
+                if destination_id in [n['url_id'] for n in flat_tree_nodes]:
+                    continue  # Do not add node to tree
+
+                # No cycle found, add to tree
+                flat_tree_nodes.append({
+                    "url_id": destination_id,
+                    "parent": nid
+                })
+
         analysis["flat_tree"] = flat_tree_nodes
         return analysis
-
 
     def run_task(self, task):
         # Initialize args list for docker
@@ -224,7 +336,10 @@ class Command(BaseCommand):
         # FIXME: read this from a static config file before
         # guessing.
         try:
-            d0_address = netifaces.ifaddresses('docker0')[netifaces.AF_INET][0]['addr']
+            d0_address = netifaces.ifaddresses(
+                'docker0'
+            )[netifaces.AF_INET][0]['addr']
+
             args.extend(['-D', '{}:27017'.format(d0_address)])
         except:
             logger.critical("Unable to get docker0 address, aborting")
@@ -293,35 +408,47 @@ class Command(BaseCommand):
         # Add URL to args
         args.append(task.url)
 
-        logger.debug("[{}] Will run command: {}".format(task.id, " ".join(args)))
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logger.debug(
+            "[{}] Will run command: {}".format(task.id, " ".join(args)))
+        p = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        # Set up a timeout of 10 minutes
         signal.signal(signal.SIGALRM, timeout_handler)
-        signal.alarm(10*60) # 10 minutes
+        signal.alarm(10*60)
+
         try:
             stdout, stderr = p.communicate()
             signal.alarm(0)  # reset the alarm
         except TimeoutException:
-            logger.error("[{}] Execution was taking too long, killed".format(task.id))
+            logger.error(
+                "[{}] Execution was taking too long, killed".format(task.id))
             raise
 
         r = re.search(r'\[MongoDB\] Analysis ID: ([a-z0-9]+)\b', stdout)
         if r:
-            logger.info("[{}] Got ObjectID: {}".format(task.id, r.group(1)))
+            logger.info(
+                "[{}] Got ObjectID: {}".format(task.id, r.group(1)))
             analysis = self.club_collections(r.group(1))
-            analysis = self.make_flat_tree(analysis,r.group(1))
+            analysis = self.make_flat_tree(analysis, r.group(1))
             analysis["frontend_id"] = str(task.frontend_id)
             final_id = db.analysiscombo.insert(analysis)
             return final_id
         else:
-            logger.error("[{}] Unable to get MongoDB analysis ID for the current task".format(task.id))
-            raise InvalidMongoIdException("Unable to get MongoDB analysis ID for the current task")
-
-
+            logger.error(
+                "[{}] Unable to get MongoDB analysis ID for the current "
+                "task".format(task.id))
+            raise InvalidMongoIdException(
+                "Unable to get MongoDB analysis ID for the current task")
 
     def handle(self, *args, **options):
         logger.info("Starting up run_thug daemon")
         # Reset any tasks left behind from previous runs
-        logger.debug("Resetting any tasks left in STATUS_PROCESSING by previous runs")
+        logger.debug(
+            "Resetting any tasks left in STATUS_PROCESSING by previous runs")
         self._reset_processing()
 
         # Start main thread
@@ -334,24 +461,31 @@ class Command(BaseCommand):
                 try:
                     task.object_id = self.run_task(task)
                 except subprocess.CalledProcessError as e:
-                    logger.exception("[{}] Got CalledProcessError exception: {}".format(task.id, e))
+                    logger.exception(
+                        "[{}] Got CalledProcessError exception: {}".format(
+                            task.id, e))
                     self._mark_as_failed(task)
                     continue
                 except TimeoutException as e:
-                    logger.exception("[{}] Got Timeout exception: {}".format(task.id, e))
+                    logger.exception(
+                        "[{}] Got Timeout exception: {}".format(
+                            task.id, e))
                     self._mark_as_failed(task)
                     continue
                 except InvalidMongoIdException as e:
-                    logger.exception("[{}] Got InvalidMongoIdException exception: {}".format(task.id, e))
+                    logger.exception(
+                        "[{}] Got InvalidMongoIdException exception: {}".format(
+                            task.id, e))
                     self._mark_as_failed(task)
                     continue
                 except Exception as e:
-                    logger.exception("[{}] Got exception: {}".format(task.id, e))
+                    logger.exception(
+                        "[{}] Got exception: {}".format(
+                            task.id, e))
                     self._mark_as_failed(task)
                     continue
 
                 self._mark_as_completed(task)
 
-            # Sleep for 10 seconds when no pending tasks
-            logger.info("Sleeping for {} seconds waiting for new tasks".format(10))
+            logger.info("Sleeping for 10 seconds waiting for new tasks")
             time.sleep(10)
